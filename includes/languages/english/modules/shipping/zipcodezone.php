812c1a4735773c7efcf6159e956a78a38b407824
drillsar <travelagloo@aol.com>
2018-01-19 14:30:01 -0500

    Initial Release

---
+++ includes/languages/english/modules/shipping/zipcodezone.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package shippingMethod
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: zones.php 4805 2006-10-22 02:32:33Z ajeh $
+ * Modified for zipcodezone usage by John Taylor - http://www.greatcanada.biz
+ * 1/14/2018 - for Zen-Cart 1.5.5 by tmccaff
+ */
+
+
+define('MODULE_SHIPPING_ZIPCODEZONE_TEXT_TITLE', 'Ground delivery');
+define('MODULE_SHIPPING_ZIPCODEZONE_TEXT_DESCRIPTION', 'Zipcode Zone Based Percentage Rates');
+define('MODULE_SHIPPING_ZIPCODEZONE_TEXT_WAY', 'Shipping to');
+define('MODULE_SHIPPING_ZIPCODEZONE_TEXT_UNITS', 'lb(s)');
+define('MODULE_SHIPPING_ZIPCODEZONE_INVALID_ZONE', 'No shipping available to the selected country');
+define('MODULE_SHIPPING_ZIPCODEZONE_UNDEFINED_RATE', 'The shipping rate cannot be determined at this time');
+?>
---
+++ includes/languages/english/modules/shipping/ziprush.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package shippingMethod
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: zones.php 4805 2006-10-22 02:32:33Z ajeh $
+ * Modified for zipcodezone usage by John Taylor - http://www.greatcanada.biz
+ * 1/14/2018 - for Zen-Cart 1.5.5 by tmccaff
+ */
+
+
+define('MODULE_SHIPPING_ZIPRUSH_TEXT_TITLE', 'Rush Delivery');
+define('MODULE_SHIPPING_ZIPRUSH_TEXT_DESCRIPTION', 'Zipcode Zone Based Percentage Rush Rates');
+define('MODULE_SHIPPING_ZIPRUSH_TEXT_WAY', 'Shipping to');
+define('MODULE_SHIPPING_ZIPRUSH_TEXT_UNITS', 'lb(s)');
+define('MODULE_SHIPPING_ZIPRUSH_INVALID_ZONE', 'No shipping available to the selected country');
+define('MODULE_SHIPPING_ZIPRUSH_UNDEFINED_RATE', 'The shipping rate cannot be determined at this time');
+?>
